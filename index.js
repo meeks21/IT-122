224579581b3a84617180ef219c38c4a990c6141b
meeks21 <anthony.meeks3@gmail.com>
2020-07-08 19:01:09 -0700

    first commit for assignment 2

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ const guitars = require('./data');//imports the data.js page as a module
 
 let guitarArray = guitars.getAll();//cretates a variable that calls the function from from the data.js page. Which will return the contents of the array.
 
-http.createServer(
-    (req,res) => {
 
-       const path = req.url.toLowerCase();
+/******************* Adds express and body-parser***************************/
+const express = require("express");
+const bodyParser = require("body-parser")
 
-       switch(path) {
-          case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Home page\n' + 'Guitar\'s in stock: ' + guitarArray.length);//this grabs the the length of the array
-            break;
-          case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About page\n My name Kemar and this is my third quarter at Seattle Central \n I\'m a little nervous about this class');
-            break;
-          default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
-            break;
-          }
-    }
- ).listen(process.env.PORT || 3000);
+
+
+
+/******************* Assignment 1 code ********************************************************************************/
+
+// http.createServer(
+//     (req,res) => {
+
+//        const path = req.url.toLowerCase();
+
+//        switch(path) {
+//           case '/':
+//             res.writeHead(200, {'Content-Type': 'text/plain'});
+//             res.end('Home page\n' + 'Guitar\'s in stock: ' + guitarArray.length);//this grabs the the length of the array
+//             break;
+//           case '/about':
+//             res.writeHead(200, {'Content-Type': 'text/plain'});
+//             res.end('About page\n My name Kemar and this is my third quarter at Seattle Central \n I\'m a little nervous about this class');
+//             break;
+//           default:
+//             res.writeHead(404, {'Content-Type': 'text/plain'});
+//             res.end('Not found');
+//             break;
+//           }
+//     }
+//  ).listen(process.env.PORT || 3000);
+
+/********************* Assignment 2 *********************************************************************************** */
